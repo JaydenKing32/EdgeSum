4e3b1c0b9bbc5dc09232cf9808ad55cd9544d4c7
JaydenKing32 <jayden.king@mq.edu.au>
2020-02-22 17:35:11 +1100

    Created DashTools to abstract dashcam methods

--- app/src/main/java/com/example/edgesum/util/dashcam/DownloadTask.java
+++ app/src/main/java/com/example/edgesum/util/dashcam/DownloadTask.java
@@ -1,37 +1,12 @@
 package com.example.edgesum.util.dashcam;
 
 import android.content.Context;
-import android.media.MediaScannerConnection;
 import android.os.AsyncTask;
-import android.util.Log;
 
-import com.example.edgesum.event.AddEvent;
-import com.example.edgesum.event.Type;
-import com.example.edgesum.model.Video;
-import com.example.edgesum.util.file.FileManager;
-import com.example.edgesum.util.video.VideoManager;
-
-import org.apache.commons.io.FileUtils;
-import org.greenrobot.eventbus.EventBus;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-
-import java.io.File;
-import java.io.IOException;
 import java.lang.ref.WeakReference;
-import java.net.ConnectException;
-import java.net.SocketTimeoutException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Function;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class DownloadTask extends AsyncTask<String, Void, List<String>> {
-    private static final String TAG = DownloadTask.class.getSimpleName();
     private final WeakReference<Context> weakReference;
 
     public DownloadTask(Context context) {
@@ -41,99 +16,10 @@ public class DownloadTask extends AsyncTask<String, Void, List<String>> {
     @Override
     protected List<String> doInBackground(String... strings) {
         // Dride
-        return downloadAll("http://192.168.1.254/DCIM/MOVIE/", "", this::getDrideFilenames);
+        return DashTools.downloadAll("http://192.168.1.254/DCIM/MOVIE/", "", DashTools::getDrideFilenames,
+                weakReference.get());
 
         // BlackVue
-//        return downloadAll("http://10.99.77.1/", "Record/", this::getBlackvueFilenames);
-    }
-
-    private List<String> downloadAll(String baseUrl, String upFolder, Function<String, List<String>> getFilenameFunc) {
-        List<String> allFiles = getFilenameFunc.apply(baseUrl);
-
-        if (allFiles == null) {
-            return null;
-        }
-        int last_n = 2;
-        List<String> lastFiles = allFiles.subList(Math.max(allFiles.size(), 0) - last_n, allFiles.size());
-
-        for (String filename : lastFiles) {
-            downloadVideo(baseUrl, upFolder, FileManager.RAW_FOOTAGE_VIDEOS_PATH.getAbsolutePath(), filename);
-        }
-        Log.i(TAG, "All downloads complete");
-        return lastFiles;
-    }
-
-    private void downloadVideo(String baseUrl, String upFolder, String downFolder, String filename) {
-        try {
-            File videoFile = new File(String.format("%s/%s", downFolder, filename));
-            Log.d(TAG, "Started downloading: " + filename);
-            FileUtils.copyURLToFile(
-                    new URL(baseUrl + upFolder + filename),
-                    videoFile
-            );
-            Context context = weakReference.get();
-
-            if (context != null) {
-                // New files aren't immediately added to the MediaStore database, so it's necessary manually trigger it
-                // Tried using sendBroadcast, but that doesn't guarantee that it will be immediately added.
-                // MediaScannerConnection ensures that the new file is added to the database before it is queried
-                // https://stackoverflow.com/a/5814533/8031185
-                MediaScannerConnection.scanFile(context,
-                        new String[]{videoFile.getAbsolutePath()}, null, (path, uri) -> {
-                            Video video = VideoManager.getVideoFromFile(context, new File(path));
-                            EventBus.getDefault().post(new AddEvent(video, Type.RAW));
-                            Log.d(TAG, "Finished downloading: " + filename);
-                        });
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private List<String> getDrideFilenames(String url) {
-        Document doc = null;
-
-        try {
-            doc = Jsoup.connect(url).get();
-        } catch (SocketTimeoutException | ConnectException e) {
-            Log.e(TAG, "Could not connect to dashcam");
-            return null;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        List<String> allFiles = new ArrayList<>();
-
-        for (Element file : doc.select("td:eq(0) > a")) {
-            if (file.text().endsWith("MP4")) {
-                allFiles.add(file.text());
-            }
-        }
-        allFiles.sort(Comparator.comparing(String::toString));
-        return allFiles;
-    }
-
-    private List<String> getBlackvueFilenames(String url) {
-        Document doc = null;
-
-        try {
-            doc = Jsoup.connect(url + "blackvue_vod.cgi").get();
-        } catch (SocketTimeoutException | ConnectException e) {
-            Log.e(TAG, "Could not connect to dashcam");
-            return null;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        List<String> allFiles = new ArrayList<>();
-
-        String raw = doc.select("body").text();
-        Pattern pat = Pattern.compile(Pattern.quote("Record/") + "(.*?)" + Pattern.quote(",s:"));
-        Matcher match = pat.matcher(raw);
-
-        while (match.find()) {
-            allFiles.add(match.group(1));
-        }
-
-        allFiles.sort(Comparator.comparing(String::toString));
-        return allFiles;
+//        return downloadAll("http://10.99.77.1/", "Record/", this::getBlackvueFilenames, weakReference.get());
     }
 }
