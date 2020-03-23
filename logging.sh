c3b25ae2c4013e7a0d666f1d3983ec5f9ddb55a7
JaydenKing32 <jayden.king@mq.edu.au>
2020-03-23 21:03:57 +1100

    Logging script can now handle multiple devices

--- logging.sh
+++ logging.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+# https://stackoverflow.com/a/2173421/8031185
+trap 'trap - SIGTERM && kill -- -$$' SIGINT SIGTERM EXIT
+
 # Get adb.exe devices output, remove \r and "device", skip first line
 devices=$(tail -n +2 <<<"$(adb.exe devices | sed -r 's/(device)?\r$//')")
 
 for serial in ${devices}; do
   pid="$(adb.exe -s "${serial}" shell ps | awk '/com\.example\.edgesum/ {print $2}')"
-  adb.exe -s "${serial}" logcat --pid "${pid}" "*:W" >"${serial}.log"
+  adb.exe -s "${serial}" logcat --pid "${pid}" "*:W" >"${serial}.log" &
 done
+
+wait
