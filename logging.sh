b3723a18a54a04bd3206472db26aa07fc2ccae1f
JaydenKing32 <jayden.king@mq.edu.au>
2020-03-31 00:04:54 +1100

    Improved logging.sh, can now specify device serial number and log verbosity

--- logging.sh
+++ logging.sh
@@ -3,12 +3,67 @@
 # https://stackoverflow.com/a/2173421/8031185
 trap 'trap - SIGTERM && kill -- -$$' SIGINT SIGTERM EXIT
 
-# Get adb.exe devices output, remove \r and "device", skip first line
-devices=$(tail -n +2 <<<"$(adb.exe devices | sed -r 's/(device)?\r$//')")
+usage() {
+    printf "Usage: ./logging.sh [-s SERIAL_NUMBER] [-v V|D|I|W|E|F|S]\n"
+    exit 1
+}
 
-for serial in ${devices}; do
-  pid="$(adb.exe -s "${serial}" shell ps | awk '/com\.example\.edgesum/ {print $2}')"
-  adb.exe -s "${serial}" logcat --pid "${pid}" "*:W" >"${serial}.log" &
+serials=""
+verbosity=""
+
+while :; do
+    case $1 in
+    -s)
+        if [[ -z "${2}" ]]; then
+            printf "ERROR: No serial number specified.\n"
+            usage
+        else
+            serials="${2}"
+            shift 2
+        fi
+        ;;
+    -v)
+        if [[ -z "${2}" ]]; then
+            printf "ERROR: No verbosity specified.\n"
+            usage
+        elif [[ ! ${2} =~ ^[VDIWEFS]$ ]]; then
+            printf "ERROR: Invalid ADB logcat verbosity: %s\n" "${2}"
+            usage
+        else
+            verbosity="${2}"
+            shift 2
+        fi
+        ;;
+    "")
+        break
+        ;;
+    -?*)
+        printf "ERROR: Unknown argument: %s\n" "${1}"
+        usage
+        ;;
+    *)
+        printf "ERROR: Unknown argument: %s\n" "${1}"
+        usage
+        ;;
+    esac
+done
+
+out_dir="./out"
+
+if [[ ! -d "${out_dir}" ]]; then
+    mkdir "${out_dir}"
+fi
+if [[ -z ${serials} ]]; then
+    # Get `adb.exe devices` output, remove \r and "device", skip first line
+    serials=$(tail -n +2 <<<"$(adb.exe devices | sed -r 's/(device)?\r$//')")
+fi
+if [[ -z ${verbosity} ]]; then
+    verbosity="W"
+fi
+
+for serial in ${serials}; do
+    pid="$(adb.exe -s "${serial}" shell ps | awk '/com\.example\.edgesum/ {print $2}')"
+    adb.exe -s "${serial}" logcat --pid "${pid}" "*:${verbosity}" >"${out_dir}/${serial}.log" &
 done
 
 wait
